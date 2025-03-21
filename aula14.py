7ee1c95ce211a3a44efc63c7ddd8540559c5b993
dennisgusmao <dennis.gusmao@getrak.com.br>
2025-03-21 08:00:47 -0300

    Update: aula sobre formatação de strings

--- aula14.py
+++ aula14.py
@@ -4,6 +4,11 @@ Formatação de strings com o método format
 a = 'A'
 b = 'B'
 c = 1.1
-string = 'a={}, b={}, c={:.2f} {}'
-formato = string.format(a, b, c, 'Dennis')
+string = 'a={nome1} b={nome2} c={nome3:.2f} c2={nome3}'
+formato = string.format(
+    nome1=a, 
+    nome2=b,
+    nome3=c    
+    )
+
 print(formato)
