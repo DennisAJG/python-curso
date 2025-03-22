2dd1a98b5ca3d67df8d279224166a51f4572e11e
dennis.gusmao <dennis.gusmao@getrak.com.br>
2025-03-22 11:28:30 -0300

    aula sobre condicionais atualizado-v1

--- aula15.py
+++ aula15.py
@@ -5,3 +5,12 @@ Função input para coleta de dados
 nome = input('Qual é o seu nome: ')
 print(f'O seu nome é {nome}')
 print(f'O nome do usuário é {nome=}')
+
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'A soma entre {numero_1} e {numero_2} é {int_numero_1 + int_numero_2}')
